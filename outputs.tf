71bdf5188a6174337d24ab65ec140b6d72774406
stoffee <chris@roadkill.org>
2022-09-12 14:19:48 -1000

    all is not lost

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "vault_ssh" {
-  value = "Connect to Vault via SSH: ssh -i private.key ec2-user@${aws_instance.ssh[0].public_ip}"
+output "rhel_ssh" {
+  value = "Connect to RHEL SSH Client: ssh -i private.key ec2-user@${aws_instance.ssh[0].public_ip}"
 }
 
-output "rhel_ssh" {
-  value = "Connect to RHEL ssh host via:  ssh -i private.key ec2-user@${aws_instance.vault[0].public_ip}"
+output "vault_ssh" {
+  value = "Connect to Vault host:  ssh -i private.key ec2-user@${aws_instance.vault[0].public_ip}"
 }
 output "vault_url" {
   value = "Vault web Interface http://${aws_instance.vault[0].public_dns}:8200/ui"
@@ -12,11 +12,5 @@ output "vault_url" {
 output "ssh" {
   value = <<SSH
 value = "ssh -i signed-cert.pub -i ~/.ssh/id_rsa ec2-user@${aws_instance.ssh[0].public_dns} p
-  #Login using vault OTP one time so that it's in the audit_log
-  #grep sshd_t /var/log/audit/audit.log | audit2allow -m vault-helper > vault-helper.te
-  #make -f /usr/share/selinux/devel/Makefile vault-helper.pp
-  #semodule -i vault-helper.pp
-  #semodule -l | grep vault
-  #setenforce 1"
 SSH
 }
